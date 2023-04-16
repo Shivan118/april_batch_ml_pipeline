e23221e72f2b843b6430ea38f9f2795b026844f5
shivan <kshivan848@gmail.com>
2023-04-16 19:39:44 +0530

    data ingestion testing

--- visa/config/configuration.py
+++ visa/config/configuration.py
@@ -58,3 +58,16 @@ class Configuartion:
             return data_ingestion_config
         except Exception as e:
             raise CustomException(e,sys) from e
+    
+    def get_training_pipeline_config(self)->TrainingPipelineConfig:
+        try:
+            trainig_pipeline_config = self.config_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir = os.path.join(ROOT_DIR, 
+                                        trainig_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+                                        trainig_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+            
+            trainig_pipeline_config = TrainingPipelineConfig(artifact_dir=artifact_dir)
+            logging.info(F"Training pipeline completed: {trainig_pipeline_config}")
+            return trainig_pipeline_config
+        except Exception as e:
+            raise CustomException(e,sys) from e
--- visa/entity/config_entity.py
+++ visa/entity/config_entity.py
@@ -3,3 +3,5 @@ from collections import namedtuple
 
 DataIngestionConfig=namedtuple("DataIngestionConfig",
 ["dataset_download_url","raw_data_dir","ingested_train_dir","ingested_test_dir"])
+
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig", ["artifact_dir"])
