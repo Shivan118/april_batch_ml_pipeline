4e217d891664fc973f0a83f91ddec9b8205624bf
shivan <kshivan848@gmail.com>
2023-04-30 19:51:42 +0530

    model trainer and evaluation

--- visa/pipeline/pipeline.py
+++ visa/pipeline/pipeline.py
@@ -4,10 +4,12 @@ import numpy as np
 from visa.constant import *
 from visa.logger import logging
 from visa.entity.artifact_entity import DataIngestionArtifact
-from visa.entity.artifact_entity import DataValidationArtifact, DataTransformationArtifact
+from visa.entity.artifact_entity import DataValidationArtifact, DataTransformationArtifact, ModelTrainerArtifact, ModelEvaluationArtifact
 from visa.components.data_transformation import DataTransformation
 from visa.components.data_ingestion import DataIngestion
 from visa.components.data_validation import DataValidation
+from visa.components.model_trainer import ModelTrainer
+from visa.components.model_evaluation import ModelEvaluation
 from visa.exception import CustomException
 from datetime import date
 from collections import namedtuple
@@ -53,6 +55,27 @@ class Pipeline():
             return data_transformation.initiate_data_transformation()
         except Exception as e:
             raise CustomException(e, sys)
+    def start_model_trainer(self, data_transformation_artifact: DataTransformationArtifact) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(model_trainer_config=self.config.get_model_trainer_config(),
+                                         data_transformation_artifact=data_transformation_artifact
+                                         )
+            return model_trainer.initiate_model_trainer()
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+                               data_validation_artifact: DataValidationArtifact,
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
+        try:
+            model_eval = ModelEvaluation(
+                model_evaluation_config=self.config.get_model_evaluation_config(),
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact,
+                model_trainer_artifact=model_trainer_artifact)
+            return model_eval.initiate_model_evaluation()
+        except Exception as e:
+            raise CustomException(e, sys) from e
         
     def run_pipeline(self):
         try:
@@ -61,6 +84,10 @@ class Pipeline():
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact = data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
                                                                          data_validation_artifact=data_validation_artifact)
+            model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    data_validation_artifact=data_validation_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
 
 
         except Exception as e:
